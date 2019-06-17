4beb02bc06df76d421e4475032dc19d7fdf1d6ec
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2019-06-17 14:18:01 +0200

    Create Clone_folder_architecture.py

---
+++ Clone_folder_architecture.py
@@ -0,0 +1,7 @@
+#Recreating the folder architecture
+for dirpath, dirnames, filenames in os.walk(inputpath):
+    structure = os.path.join(outputpath, dirpath[len(inputpath):])
+    if not os.path.isdir(structure):
+        os.mkdir(structure)
+    else:
+        print("Folder does already exits!")
